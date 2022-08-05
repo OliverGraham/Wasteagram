741e95b6ded35e0401054e414c7beec28c27afbf
Oliver <grahamol@oregonstate.edu>
2022-08-05 10:21:56 -0700

    update location package, add sample location screen

---
+++ lib/screens/ShareLocationScreen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'package:flutter/services.dart';
+
+class ShareLocationScreen extends StatefulWidget {
+  const ShareLocationScreen({Key? key}) : super(key: key);
+
+  @override
+  ShareLocationScreenState createState() => ShareLocationScreenState();
+}
+
+class ShareLocationScreenState extends State<ShareLocationScreen> {
+  LocationData? locationData;
+  var locationService = Location();
+
+  @override
+  void initState() {
+    super.initState();
+    retrieveLocation();
+  }
+
+  void retrieveLocation() async {
+    try {
+      var serviceEnabled = await locationService.serviceEnabled();
+      if (!serviceEnabled) {
+        serviceEnabled = await locationService.requestService();
+        if (!serviceEnabled) {
+          print('Failed to enable service. Returning.');
+          return;
+        }
+      }
+
+      var permissionGranted = await locationService.hasPermission();
+      if (permissionGranted == PermissionStatus.denied) {
+        permissionGranted = await locationService.requestPermission();
+        if (permissionGranted != PermissionStatus.granted) {
+          print('Location service permission not granted. Returning.');
+        }
+      }
+
+      locationData = await locationService.getLocation();
+    } on PlatformException catch (e) {
+      print('Error: ${e.toString()}, code: ${e.code}');
+      locationData = null;
+    }
+    locationData = await locationService.getLocation();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (locationData == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Latitude: ${locationData!.latitude}',
+                style: Theme.of(context).textTheme.headline4),
+            Text('Longitude:  ${locationData!.longitude}',
+                style: Theme.of(context).textTheme.headline4),
+            ElevatedButton(
+              child: const Text('Share'),
+              onPressed: () {
+              },
+            )
+          ],
+        )
+    );
+  }
+}
+
