157b5f6f9da6884c8d267ac9d5d667f8958cebd9
Oliver <grahamol@oregonstate.edu>
2022-08-09 10:51:45 -0700

    performance; all videos done

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:wasteagram/screens/CameraScreen.dart';
 import 'package:wasteagram/screens/ShareLocationScreen.dart';
 
+// compute function, to run things in separate Isolate?
+
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
