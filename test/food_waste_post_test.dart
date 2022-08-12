8876a29c17a7eda6bfc9770af4ca6cf88198349d
Oliver <grahamol@oregonstate.edu>
2022-08-11 22:27:35 -0700

    add FoodWastePost unit test

--- test/food_waste_post_test.dart
+++ test/food_waste_post_test.dart
@@ -3,8 +3,8 @@ import 'package:wasteagram/models/food_waste_post.dart';
 
 
 void main() {
-  test('Post created from Map should have appropriate property values', () {
-
+  test('FoodWastePost created from Map '
+      'should have appropriate property values', () {
     // first, setup
     final date = DateTime.parse('2022-08-09');
     const url = 'FAKE';
@@ -16,7 +16,7 @@ void main() {
     // second, exercise
     final foodWastePost = FoodWastePost.fromJson({
       'date': date,
-      'photoURL': url,
+      'imageURL': url,
       'quantity': quantity,
       'latitude': latitude,
       'longitude': longitude
@@ -24,9 +24,36 @@ void main() {
 
     // third, verify
     expect(foodWastePost.date, date);
-    expect(foodWastePost.photoURL, url);
+    expect(foodWastePost.imageURL, url);
     expect(foodWastePost.quantity, quantity);
     expect(foodWastePost.latitude, latitude);
     expect(foodWastePost.longitude, longitude);
   });
+
+
+  test('Map created from FoodWastePost should have appropriate values', () {
+
+    final date = DateTime.parse('2022-08-09');
+    const url = 'FAKE';
+    const quantity = 1;
+    const latitude = 1.0;
+    const longitude = 2.0;
+
+    FoodWastePost fakePost = FoodWastePost(
+      date: date,
+      imageURL: url,
+      quantity: quantity,
+      latitude: latitude,
+      longitude: longitude
+    );
+
+    final fakeFirestoreDoc = fakePost.toJson();
+
+    expect(fakeFirestoreDoc['date'], date);
+    expect(fakeFirestoreDoc['imageURL'], url);
+    expect(fakeFirestoreDoc['quantity'], quantity);
+    expect(fakeFirestoreDoc['latitude'], latitude);
+    expect(fakeFirestoreDoc['longitude'], longitude);
+  });
+
 }
